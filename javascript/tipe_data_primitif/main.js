e6dcbbe0b4aecad798d849888bc178758d592636
ALMIRA SAKINAH <almirasakinah181@gmail.com>
2026-06-19 23:59:35 +0700

    Add files via upload
    belajar kembali string dkk pelajaran penting js hanya bisa nge run di folde yang langsung ke file js

---
+++ javascript/tipe_data_primitif/main.js
@@ -0,0 +1,21 @@
+//string
+const currentYear = new Date().getFullYear();
+const text = 'Tahun ini adalah' + currentYear;
+
+//number
+const hasil = 100/0;
+
+const number = Number('text');
+
+//boolean
+const isGreater =   100 > 3;
+
+//nilai kosong
+let message= null;
+
+//output
+console.log(hasil); //coba output infinity yaitu 
+console.log(text);
+console.log(number); //coba output NaN yaitu NaN
+console.log(isGreater);
+console.log(message);
