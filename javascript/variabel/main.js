52ada74179ca0fc62f66158aaa20db1e81438b11
ALMIRA SAKINAH <almirasakinah181@gmail.com>
2026-06-19 23:36:40 +0700

    Add files via upload
    belajar js biar bisa scripting

---
+++ javascript/variabel/main.js
@@ -0,0 +1,11 @@
+const id = 123;
+let username ='alsa';
+
+console.log(id);
+console.log(username);
+
+username = 'sal4';
+
+console.log('setelah diubah: ', username);
+
+//Note: const BISA diubah nilainya, let TIDAK bisa diubah
